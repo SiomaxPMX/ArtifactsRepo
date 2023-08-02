7a0681f3632ad21211eb61cd234867f65cdbaa2b
mabustos <dark_mabg@hotmail.com>
2023-08-01 19:41:19 -0600

    ArtifactRealease

--- InstallerSiomaxPMX7.ps1
+++ InstallerSiomaxPMX7.ps1
@@ -2,7 +2,7 @@ function Install-Siomax {
     Write-Host "Downloading Siomax 7..."
     $Path = $env:TEMP
     $Installer = "smx.exe"
-    $URL = "https://github.com/SiomaxPMX/ArtifactsRepo/releases/download/7.0.1_beta.1/Siomax7pmx_32_7.0.1_beta.1.exe"
+    $URL = "https://github.com/SiomaxPMX/ArtifactsRepo/releases/download/7.0.1_beta.2/Siomax_7.0.1.0_PMX_32.exe"
     Start-BitsTransfer -Source $URL -Destination $Path\$Installer
 
     Write-Host "Installing Siomax 7..."

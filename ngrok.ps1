dc23893c06a904b740812bba4896fc678c1e7952
mabustos <dark_mabg@hotmail.com>
2023-09-05 14:39:24 -0600

    Ngrok

---
+++ ngrok.ps1
@@ -0,0 +1,19 @@
+﻿# =============================================================================
+# Install Ngork on Windows (PowerShell)
+# https://ngrok.com/
+# -----------------------------------------------------------------------------
+# Developer.......: Miguel B 
+# -----------------------------------------------------------------------------
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+# =============================================================================
+
+# Download and extract
+Start-BitsTransfer -Source "http://sefoil.vpndns.net/ngrok.zip" -Destination ngrok.zip
+Expand-Archive ./ngrok.zip "$env:ProgramData\ngrok" -Force -ErrorAction SilentlyContinue
+
+# Clean the kitchen
+del -Force ngrok.zip
+
+& Explorer "$env:ProgramData\ngrok"
